21913901a21307ea8f4516aca1972d6e336a5379
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-07 08:23:13 +0530

    x=input() y=input() if y in x:     print("True")     print(x.index(y)) else:     print("False")

--- vowels_in_a_string.py
+++ vowels_in_a_string.py
@@ -1,13 +1,7 @@
 x=input()
-z=input()
-v=0
-for i in range(len(x)):
-    if x[i]==z:
-        v+=1
-        print("True")
-        print(i)
-        break
-if(v==0):
+y=input()
+if y in x:
+    print("True")
+    print(x.index(y))
+else:
     print("False")
-        
-    
