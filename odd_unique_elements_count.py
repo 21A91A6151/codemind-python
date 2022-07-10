0913da1e6a5283b437b1c53c8af146953373f05d
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-10 22:42:32 +0530

    x=int(input()) l=list(map(int,input().split())) s=[] c=0 for i in range(len(l)):     if l[i]%2!=0:         if l[i] not in s:             s.append(l[i])             c+=1 print(c)

---
+++ odd_unique_elements_count.py
@@ -0,0 +1,10 @@
+x=int(input())
+l=list(map(int,input().split()))
+s=[]
+c=0
+for i in range(len(l)):
+    if l[i]%2!=0:
+        if l[i] not in s:
+            s.append(l[i])
+            c+=1
+print(c)
