a9cf4f672874712fda53efc5ba5fe147ceeabb77
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-11 20:28:00 +0530

    x=int(input()) k=str(x) k=k[::-1] k=int(k) c=0 z=0 for i in range(1,x+1):     if x%i==0:         c+=1 for i in range(1,k+1):     if k%i==0:         z+=1 if z==2 and c==2:     print("circular prime") elif ((c==2 and k!=2) or (c!=2 and k==2)) :     print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,24 +1,18 @@
 x=int(input())
-temp=x
+k=str(x)
+k=k[::-1]
+k=int(k)
 c=0
-k=0
-s=0
+z=0
 for i in range(1,x+1):
     if x%i==0:
         c+=1
-    if c==2:
-        while temp>0:
-            d=temp%10
-            temp=temp//10
-            k=k*10+d
-        for j in range(1,k+1):
-            if k%j==0:
-                s+=1
-if s==2 and c==2:
+for i in range(1,k+1):
+    if k%i==0:
+        z+=1
+if z==2 and c==2:
     print("circular prime")
-elif s==2 or c==2:
+elif ((c==2 and k!=2) or (c!=2 and k==2)) :
     print("prime but not a circular prime")
 else:
     print("not prime")
-                
-        
