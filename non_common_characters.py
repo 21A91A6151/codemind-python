0342443777cba820b1f747e02a9af965fbb47021
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-13 18:39:54 +0530

    x=input() x=x.lower() y=input() y=y.lower() k=0 s=[] for i in y:     if i not in x:         if i==" ":             continue         if i not in s:             s.append(i) for i in x:     if i not in y:         if i==" ":             continue         if i not in s:             s.append(i) k=sorted(s) for i in k:     print(i,end="")

---
+++ non_common_characters.py
@@ -0,0 +1,21 @@
+x=input()
+x=x.lower()
+y=input()
+y=y.lower()
+k=0
+s=[]
+for i in y:
+    if i not in x:
+        if i==" ":
+            continue
+        if i not in s:
+            s.append(i)
+for i in x:
+    if i not in y:
+        if i==" ":
+            continue
+        if i not in s:
+            s.append(i)
+k=sorted(s)
+for i in k:
+    print(i,end="")
