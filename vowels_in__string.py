4f66c93f12c60a2b88fead13a0931418958da7d1
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-13 19:15:07 +0530

    x=input() s=[] for i in x:     if i in 'AEIOUaeiou':         if i not in s:             s.append(i) print(*s)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,11 +1,7 @@
 x=input()
-c=0
 s=[]
 for i in x:
     if i in 'AEIOUaeiou':
         if i not in s:
-            c+=1
             s.append(i)
-            print(i,end=" ")
-if(c==0):
-    print("-1")
+print(*s)
