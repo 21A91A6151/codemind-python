20afaab31da3321fe33abcaf874a86d7d5145ff0
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-13 18:32:11 +0530

    x=input() x=x.lower() y=input() y=y.lower() y=y.split() x=x.split() k=0 s=[] for i in y:     if i in x:         if i not in s:             s.append(i) print(*s)

--- common_words.py
+++ common_words.py
@@ -2,8 +2,12 @@ x=input()
 x=x.lower()
 y=input()
 y=y.lower()
-x=x.split()
 y=y.split()
+x=x.split()
+k=0
+s=[]
 for i in y:
     if i in x:
-        print(i,end=" ")
+        if i not in s:
+            s.append(i)
+print(*s)
