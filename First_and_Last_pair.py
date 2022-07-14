0b1950aced6aee8c289fe5b64077574e8f2beeea
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-14 06:34:05 +0530

    x=int(input()) l=list(map(int,input().split())) k=l[::-1] s=[] for i in range(x//2):     s.append(l[i])     s.append(k[i]) if x%2!=0:     s.append(l[x//2])     s.append("0") print(*s)

--- First_and_Last_pair.py
+++ First_and_Last_pair.py
@@ -2,14 +2,11 @@ x=int(input())
 l=list(map(int,input().split()))
 k=l[::-1]
 s=[]
-
 for i in range(x//2):
     s.append(l[i])
     s.append(k[i])
 if x%2!=0:
     s.append(l[x//2])
     s.append("0")
-        
 print(*s)
         
-    
