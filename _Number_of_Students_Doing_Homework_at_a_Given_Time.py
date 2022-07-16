70317921c9922c8ee60e57e8eeff13a3b7e82df1
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-16 07:25:37 +0530

    x=int(input()) l=list(map(int,input().split())) y=int(input()) m=list(map(int,input().split())) h=int(input()) c=0 for i in range(x):     if l[i]<=h and m[i]>=h:         c+=1 print(c)

--- _Number_of_Students_Doing_Homework_at_a_Given_Time.py
+++ _Number_of_Students_Doing_Homework_at_a_Given_Time.py
@@ -1,10 +1,11 @@
 x=int(input())
 l=list(map(int,input().split()))
 y=int(input())
-k=list(map(int,input().split()))
+m=list(map(int,input().split()))
 h=int(input())
 c=0
 for i in range(x):
-    if l[i]<=h and k[i]>=h:
+    if l[i]<=h and m[i]>=h:
         c+=1
 print(c)
+    
