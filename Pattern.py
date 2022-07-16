e979c322f800f2b7ea0b01cb6557d3e63afc495f
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-16 20:23:58 +0530

    x=int(input()) a=1 for i in range(1,x+1):     for j in range(1,x+1-i):         print(' ',end="")     for j in range(1,i+1):         print(j,end="")     for j in range(i-1,0,-1):         print(j,end="")     print()

--- Pattern.py
+++ Pattern.py
@@ -1,7 +1,10 @@
 x=int(input())
+a=1
 for i in range(1,x+1):
-    for j in range(x,0,-1):
-        print(i,end=" ")
+    for j in range(1,x+1-i):
+        print(' ',end="")
+    for j in range(1,i+1):
+        print(j,end="")
+    for j in range(i-1,0,-1):
+        print(j,end="")
     print()
-    
-        
