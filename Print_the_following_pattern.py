0960cdff558f61832c48decdbe77936cdd8187af
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-07-16 20:21:41 +0530

    x=int(input()) a=64 for i in range(1,x+1):     for j in range(1,x+1-i):         print(' ',end="")     for j in range(1,i+1):         print('%c'%(a+j),end="")     for j in range(i-1,0,-1):         print('%c'%(a+j),end="")     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,5 +1,10 @@
 x=int(input())
+a=64
 for i in range(1,x+1):
-    for j in range(i,x+1):
-        print(j,end=" ")
+    for j in range(1,x+1-i):
+        print(' ',end="")
+    for j in range(1,i+1):
+        print('%c'%(a+j),end="")
+    for j in range(i-1,0,-1):
+        print('%c'%(a+j),end="")
     print()
