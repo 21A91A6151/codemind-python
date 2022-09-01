d822c1762124ed8b1edee2545397971128f29a36
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-09-01 21:31:14 +0530

    x=input() x=x.lower() s=x.split() c=0 k=0 for i in s:     for j in range(len(i)//2):         if(i[j] in 'AEIOUaeiou') and i[-(j+1)] not in 'AEIOUaeiou)':             c+=1         if(i[j] not in 'AEIOUaeiou') and i[-(j+1)] in 'AEIOUaeiou)':             c+=1 print(c)

---
+++ count_vowel_consonent_pairs_in_words.py
@@ -0,0 +1,13 @@
+x=input()
+x=x.lower()
+s=x.split()
+c=0
+k=0
+for i in s:
+    for j in range(len(i)//2):
+        if(i[j] in 'AEIOUaeiou') and i[-(j+1)] not in 'AEIOUaeiou)':
+            c+=1
+        if(i[j] not in 'AEIOUaeiou') and i[-(j+1)] in 'AEIOUaeiou)':
+            c+=1
+print(c)
+            
